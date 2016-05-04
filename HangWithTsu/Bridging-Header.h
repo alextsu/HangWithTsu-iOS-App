546479db339528002ead7129c002abf9b69b9c00
alextsu <alextsu@gmail.com>
2016-05-03 23:53:14 -0400

    first commit

---
+++ HangWithTsu/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  HangWithTsu
+//
+//  Created by Alexander Tsu on 5/2/16.
+//  Copyright © 2016 Alexander Tsu. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#endif /* Bridging_Header_h */
---
+++ Podfile
@@ -0,0 +1,8 @@
+workspace 'HangWithTsu'
+
+use_frameworks!
+
+target 'HangWithTsu' do
+	pod 'Gifu'
+	pod 'FoldingCell', '~> 0.8.1'
+end
